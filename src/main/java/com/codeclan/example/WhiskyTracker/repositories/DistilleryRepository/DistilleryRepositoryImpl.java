6e5e7c34724da6fc4acc12c3362376cfeb13bf66
davemc85 <davemcallister@hotmail.co.uk>
2019-06-12 15:08:08 +0100

    distillery repositories set up

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/DistilleryRepository/DistilleryRepositoryImpl.java
@@ -1,4 +1,38 @@
 package com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class DistilleryRepositoryImpl implements DistilleryRepositoryCustom {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Distillery> findDistilleriesFromParticularRegion(String region){
+        List<Distillery> result = null;
+        Session session = entityManager.unwrap(Session.class);
+
+        try {
+            Criteria cr = session.createCriteria(Distillery.class);
+            cr.createAlias("distilleries", "distilleryAlias");
+            cr.add(Restrictions.eq("distilleryAlias.region", region));
+            result = cr.list();
+        } catch (HibernateException ex){
+            ex.printStackTrace();
+        } finally {
+
+        }
+        return result;
+
+    }
+
 }
